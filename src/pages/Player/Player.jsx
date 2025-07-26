13db49564831d83fda31158cfae5ea54d42b016c
Abuzar khan <aburaja2654@gmail.com>
2025-07-26 16:12:41 +0530

    new changes

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -35,7 +35,7 @@ const Player = () => {
 
   return (
     <div className="player">
-      <img src={backIcon} alt="" onClick={()=>{navigate(-2)}}/>
+      <img src={backIcon} alt="" onClick={()=>{navigate('/Home')}}/>
       <iframe
         width="90%"
         height="90%"
